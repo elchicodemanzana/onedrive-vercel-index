d549fda2d4ae62781169fe42a349e1b03bbea6b9
elchicodemanzana <148639343+elchicodemanzana@users.noreply.github.com>
2024-01-25 17:53:58 +0100

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'Discord,
+      name: 'Discord',
       link: 'https://discord.gg/3KaejNVu',
     },
   ],
