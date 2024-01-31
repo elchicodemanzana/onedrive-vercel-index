b054335f2afd1809447f342fb9816a769b509b19
elchicodemanzana <148639343+elchicodemanzana@users.noreply.github.com>
2024-01-31 23:02:56 +0100

    Update FolderListLayout.tsx

--- src/components/FolderListLayout.tsx
+++ src/components/FolderListLayout.tsx
@@ -14,7 +14,6 @@ import { getStoredToken } from '../utils/protectedRouteHandler'
 
 const FileListItem: FC<{ fileContent: OdFolderChildren }> = ({ fileContent: c }) => {
   return (
-    <div className="grid cursor-pointer grid-cols-10 items-center space-x-2 px-3 py-2.5">
       <div className="col-span-10 flex items-center space-x-2 truncate md:col-span-6" title={c.name}>
         <div className="w-5 flex-shrink-0 text-center">
           <ChildIcon child={c} />
@@ -59,6 +58,7 @@ const FolderListLayout = ({
 
   return (
     <div className="rounded bg-white shadow-sm dark:bg-gray-900 dark:text-gray-100">
+      <div className="grid grid-cols-12 items-center space-x-2 border-b border-gray-900/10 px-3 dark:border-gray-500/30">
         <div className="col-span-12 py-2 text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:col-span-6">
           {t('Name')}
         </div>
@@ -71,6 +71,10 @@ const FolderListLayout = ({
       </div>
 
       {folderChildren.map((c: OdFolderChildren) => (
+        <div
+          className="grid grid-cols-12 transition-all duration-100 hover:bg-gray-100 dark:hover:bg-gray-850"
+          key={c.id}
+        >
           <div className="col-span-12 md:col-span-10">
             {/* Dodaj obsługę nawigacji za pomocą komponentu Link */}
             <Link href={`${path === '/' ? '' : path}/${encodeURIComponent(c.name)}`} passHref>
