505a58eb37ac0a3c90f19993c2fcce91051e2505
elchicodemanzana <148639343+elchicodemanzana@users.noreply.github.com>
2024-01-31 22:04:38 +0100

    Update FileListing.tsx

--- src/components/FileListing.tsx
+++ src/components/FileListing.tsx
@@ -2,19 +2,22 @@ import type { OdFileObject, OdFolderChildren, OdFolderObject } from '../types'
 import { ParsedUrlQuery } from 'querystring'
 import { FC, MouseEventHandler, SetStateAction, useEffect, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Toaster } from 'react-hot-toast'; // Added import for Toaster
+import { Toaster, useToaster } from 'react-hot-toast'; // Added import for Toaster and useToaster
 import emojiRegex from 'emoji-regex'
-
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
-
 import useLocalStorage from '../utils/useLocalStorage'
 import { getPreviewType, preview } from '../utils/getPreviewType'
 import { useProtectedSWRInfinite } from '../utils/fetchWithSWR'
 import { getExtension, getRawExtension, getFileIcon } from '../utils/getFileIcon'
 import { getStoredToken } from '../utils/protectedRouteHandler'
-
+import {
+  DownloadingToast,
+  downloadMultipleFiles,
+  downloadTreelikeMultipleFiles,
+  traverseFolder,
+} from './MultiFileDownloader'
 import { layouts } from './SwitchLayout'
 import Loading, { LoadingIcon } from './Loading'
 import FourOhFour from './FourOhFour'
@@ -30,7 +33,6 @@ import URLPreview from './previews/URLPreview'
 import ImagePreview from './previews/ImagePreview'
 import DefaultPreview from './previews/DefaultPreview'
 import { PreviewContainer } from './previews/Containers'
-
 import FolderListLayout from './FolderListLayout'
 import FolderGridLayout from './FolderGridLayout'
 
@@ -60,10 +62,12 @@ const renderEmoji = (name: string) => {
   const emoji = emojiRegex().exec(name)
   return { render: emoji && !emoji.index, emoji }
 }
+
 const formatChildName = (name: string) => {
   const { render, emoji } = renderEmoji(name)
   return render ? name.replace(emoji ? emoji[0] : '', '').trim() : name
 }
+
 export const ChildName: FC<{ name: string; folder?: boolean }> = ({ name, folder }) => {
   const original = formatChildName(name)
   const extension = folder ? '' : getRawExtension(original)
@@ -74,6 +78,7 @@ export const ChildName: FC<{ name: string; folder?: boolean }> = ({ name, folder
     </span>
   )
 }
+
 export const ChildIcon: FC<{ child: OdFolderChildren }> = ({ child }) => {
   const { render, emoji } = renderEmoji(child.name)
   return render ? (
@@ -132,8 +137,6 @@ export const Downloading: FC<{ title: string; style: string }> = ({ title, style
   return (
     <span title={title} className={`${style} rounded`} role="status">
       <LoadingIcon
-        // Use fontawesome far theme via class `svg-inline--fa` to get style `vertical-align` only
-        // for consistent icon alignment, as class `align-*` cannot satisfy it
         className="svg-inline--fa inline-block h-4 w-4 animate-spin"
       />
     </span>
@@ -141,6 +144,13 @@ export const Downloading: FC<{ title: string; style: string }> = ({ title, style
 }
 
 const FileListing: FC<{ query?: ParsedUrlQuery }> = ({ query }) => {
+  const [selected, setSelected] = useState<{ [key: string]: boolean }>({})
+  const [totalSelected, setTotalSelected] = useState<0 | 1 | 2>(0)
+  const [totalGenerating, setTotalGenerating] = useState<boolean>(false)
+  const [folderGenerating, setFolderGenerating] = useState<{
+    [key: string]: boolean
+  }>({})
+
   const router = useRouter()
   const hashedToken = getStoredToken(router.asPath)
   const [layout, _] = useLocalStorage('preferredLayout', layouts[0])
@@ -150,6 +160,9 @@ const FileListing: FC<{ query?: ParsedUrlQuery }> = ({ query }) => {
   const path = queryToPath(query)
 
   const { data, error, size, setSize } = useProtectedSWRInfinite(path)
+
+  // Use useToaster hook to get the toast function
+  const toast = useToaster();
 
   if (error) {
     // If error includes 403 which means the user has not completed initial setup, redirect to OAuth page
@@ -186,6 +199,129 @@ const FileListing: FC<{ query?: ParsedUrlQuery }> = ({ query }) => {
 
     // Find README.md file to render
     const readmeFile = folderChildren.find(c => c.name.toLowerCase() === 'readme.md')
+
+    // Filtered file list helper
+    const getFiles = () => folderChildren.filter(c => !c.folder && c.name !== '.password')
+
+    // File selection
+    const genTotalSelected = (selected: { [key: string]: boolean }) => {
+      const selectInfo = getFiles().map(c => Boolean(selected[c.id]))
+      const [hasT, hasF] = [selectInfo.some(i => i), selectInfo.some(i => !i)]
+      return hasT && hasF ? 1 : !hasF ? 2 : 0
+    }
+
+    const toggleItemSelected = (id: string) => {
+      let val: SetStateAction<{ [key: string]: boolean }>
+      if (selected[id]) {
+        val = { ...selected }
+        delete val[id]
+      } else {
+        val = { ...selected, [id]: true }
+      }
+      setSelected(val)
+      setTotalSelected(genTotalSelected(val))
+    }
+
+    const toggleTotalSelected = () => {
+      if (genTotalSelected(selected) == 2) {
+        setSelected({})
+        setTotalSelected(0)
+      } else {
+        setSelected(Object.fromEntries(getFiles().map(c => [c.id, true])))
+        setTotalSelected(2)
+      }
+    }
+
+    // Selected file download
+    const handleSelectedDownload = () => {
+      const folderName = path.substring(path.lastIndexOf('/') + 1)
+      const folder = folderName ? decodeURIComponent(folderName) : undefined
+      const files = getFiles()
+        .filter(c => selected[c.id])
+        .map(c => ({
+          name: c.name,
+          url: `/api/raw/?path=${path}/${encodeURIComponent(c.name)}${hashedToken ? `&odpt=${hashedToken}` : ''}`,
+        }))
+
+      if (files.length == 1) {
+        const el = document.createElement('a')
+        el.style.display = 'none'
+        document.body.appendChild(el)
+        el.href = files[0].url
+        el.click()
+        el.remove()
+      } else if (files.length > 1) {
+        setTotalGenerating(true)
+
+        const toastId = toast.loading(<DownloadingToast router={router} />)
+        downloadMultipleFiles({ toastId, router, files, folder })
+          .then(() => {
+            setTotalGenerating(false)
+            toast.success(t('Finished downloading selected files.'), {
+              id: toastId,
+            })
+          })
+          .catch(() => {
+            setTotalGenerating(false)
+            toast.error(t('Failed to download selected files.'), { id: toastId })
+          })
+      }
+    }
+
+    // Get selected file permalink
+    const handleSelectedPermalink = (baseUrl: string) => {
+      return getFiles()
+        .filter(c => selected[c.id])
+        .map(
+          c =>
+            `${baseUrl}/api/raw/?path=${path}/${encodeURIComponent(c.name)}${hashedToken ? `&odpt=${hashedToken}` : ''}`
+        )
+        .join('\n')
+    }
+
+    // Folder recursive download
+    const handleFolderDownload = (path: string, id: string, name?: string) => () => {
+      const files = (async function* () {
+        for await (const { meta: c, path: p, isFolder, error } of traverseFolder(path)) {
+          if (error) {
+            toast.error(
+              t('Failed to download folder {{path}}: {{status}} {{message}} Skipped it to continue.', {
+                path: p,
+                status: error.status,
+                message: error.message,
+              })
+            )
+            continue
+          }
+          const hashedTokenForPath = getStoredToken(p)
+          yield {
+            name: c?.name,
+            url: `/api/raw/?path=${p}${hashedTokenForPath ? `&odpt=${hashedTokenForPath}` : ''}`,
+            path: p,
+            isFolder,
+          }
+        }
+      })()
+
+      setFolderGenerating({ ...folderGenerating, [id]: true })
+      const toastId = toast.loading(<DownloadingToast router={router} />)
+
+      downloadTreelikeMultipleFiles({
+        toastId,
+        router,
+        files,
+        basePath: path,
+        folder: name,
+      })
+        .then(() => {
+          setFolderGenerating({ ...folderGenerating, [id]: false })
+          toast.success(t('Finished downloading folder.'), { id: toastId })
+        })
+        .catch(() => {
+          setFolderGenerating({ ...folderGenerating, [id]: false })
+          toast.error(t('Failed to download folder.'), { id: toastId })
+        })
+    }
 
     // Folder layout component props
     const folderProps = {
@@ -304,5 +440,4 @@ const FileListing: FC<{ query?: ParsedUrlQuery }> = ({ query }) => {
     </PreviewContainer>
   )
 }
-
 export default FileListing;
